7f7d31289efc16eead63daf45ccfff30c06f0de8
Szymon Miękus <miekusoft@gmail.com>
2022-08-13 00:43:34 +0200

    Debugging approach

---
+++ debugging/routes.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<h1>home path</h1>");
+    res.write(
+      "<form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>klik</button></form>"
+    );
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        console.log(parsedBody);
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<h1>other path</h1>");
+  res.end();
+};
+
+// exports.handler = requestHandler;
+//OR
+// module.exports.handler = requestHandler;
+//OR
+module.exports = { handler: requestHandler };
+
+// module.exports = requestHandler; //default
