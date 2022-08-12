5473c68c12c07dc7cd8fa1da252143b9af395187
Szymon Miękus <miekusoft@gmail.com>
2022-08-12 22:54:37 +0200

    Initial commit

---
+++ basic_node_server/app.js
@@ -0,0 +1,40 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write("<h1>home path</h1>");
+    res.write(
+      "<form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>klik</button></form>"
+    );
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+      console.log(parsedBody);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<h1>other path</h1>");
+  res.end();
+});
+
+server.listen(3000);
---
+++ promise/sandbox.js
@@ -0,0 +1,23 @@
+const fetchData = () => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Done");
+    }, 1500);
+  });
+  return promise;
+};
+
+setTimeout(() => {
+  console.log("Timer is done");
+  fetchData()
+    .then((text1) => {
+      console.log(text1);
+      return fetchData();
+    })
+    .then((text2) => {
+      console.log(text2);
+    });
+}, 2000);
+
+console.log("First");
+console.log("Second");
