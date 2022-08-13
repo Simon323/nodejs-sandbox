6827e0477ec9c6780a056a0f68f797cd9ba4aa66
Szymon Miękus <miekusoft@gmail.com>
2022-08-13 22:04:57 +0200

    Express middleware sandbox

--- express_node_server/app.js
+++ express_node_server/app.js
@@ -1,18 +1,31 @@
 const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
-  next();
+// app.use("/", (req, res, next) => {
+//   console.log("This always run");
+//   next();
+// });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/add-product", (req, res, next) => {
+  console.log("This is another middleware");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title' /><button type='submit'>submit</button></form>"
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("Another the middleware");
-  next();
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
-const server = http.createServer(app);
+app.use("/", (req, res, next) => {
+  console.log("This is another middleware");
+  res.send("<h1>Hello form expres</h1>");
+});
 
-server.listen(3000);
+app.listen(3000);
