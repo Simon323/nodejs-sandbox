4eff812b2428b9674719476f5c74ce6cfbe1ddab
Szymon Miękus <miekusoft@gmail.com>
2022-08-15 00:08:12 +0200

    Refactor model logic

--- mvc_app/models/product.js
+++ mvc_app/models/product.js
@@ -1,6 +1,17 @@
 const fs = require("fs");
 const path = require("path");
 const rootDir = require("../util/path");
+const p = path.join(rootDir, "data", "products.json");
+
+const getProductsFromFile = (callback) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      callback([]);
+    } else {
+      callback(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
   constructor(title) {
@@ -8,12 +19,7 @@ module.exports = class Product {
   }
 
   save() {
-    const p = path.join(rootDir, "data", "products.json");
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
@@ -22,13 +28,6 @@ module.exports = class Product {
   }
 
   static fetchAll(callback) {
-    const p = path.join(rootDir, "data", "products.json");
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        callback([]);
-      } else {
-        callback(JSON.parse(fileContent));
-      }
-    });
+    getProductsFromFile(callback);
   }
 };
