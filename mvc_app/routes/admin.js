dd8b50c6f532582d5e58b2e2d94a86b65e637052
Szymon Miękus <miekusoft@gmail.com>
2022-08-15 00:39:09 +0200

    Prepare to handle new pages

--- mvc_app/routes/admin.js
+++ mvc_app/routes/admin.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { getAddProduct, postAddProduct } = require("../controllers/products");
+const { getAddProduct, postAddProduct } = require("../controllers/shop");
 
 const router = express.Router();
 
 router.get("/add-product", getAddProduct);
 
 router.post("/add-product", postAddProduct);
+
+router.get("/products", postAddProduct);
 
 module.exports = router;
--- mvc_app/routes/shop.js
+++ mvc_app/routes/shop.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { getProducts } = require("../controllers/products");
+const { getProducts } = require("../controllers/shop");
 
 const router = express.Router();
 
 router.get("/", getProducts);
+
+router.get("/products", getProducts);
+
+router.get("/cart", getProducts);
+
+router.get("/checkout", getProducts);
 
 module.exports = router;
