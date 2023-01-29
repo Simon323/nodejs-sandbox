c528fa1c28d86ed184dfe2cdee2b5c5281367e49
Szymon Miękus <miekusoft@gmail.com>
2023-01-29 23:36:31 +0100

    mock fetch request

--- typescript_sandbox/src/__test__/fetch_with_retry.test.ts
+++ typescript_sandbox/src/__test__/fetch_with_retry.test.ts
@@ -1,6 +1,33 @@
 import { __greeter } from "../fetch_with_retry";
 import { HELLO_MESSAGE } from "./__mocks__/constatnts";
+const fetchMock = require('fetch-mock');
+
+interface LoadTest {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean
+}
+
+fetchMock.get('https://jsonplaceholder.typicode.com/todos/1', {
+  userId: 1,
+  id: 1,
+  title: "delectus aut autem",
+  completed: false
+} as LoadTest);
 
 test("My Greeter", () => {
   expect(__greeter("Carl")).toBe(HELLO_MESSAGE);
 });
+
+test("Mock http response", async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  const result = await response.json()
+  
+  expect(result).toEqual({
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false
+  })
+})
