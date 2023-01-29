349a26b17db760bab7ad920375e2061bc6d2bd2c
Szymon Miękus <miekusoft@gmail.com>
2023-01-30 00:00:45 +0100

    Format test file

--- typescript_sandbox/src/__test__/fetch_with_retry.test.ts
+++ typescript_sandbox/src/__test__/fetch_with_retry.test.ts
@@ -1,33 +1,53 @@
-import { __greeter } from "../fetch_with_retry";
+import fetchWithRetry, { __greeter } from "../fetch_with_retry";
 import { HELLO_MESSAGE } from "./__mocks__/constatnts";
-const fetchMock = require('fetch-mock');
+const fetchMock = require("fetch-mock");
 
 interface LoadTest {
-  userId: number,
-  id: number,
-  title: string,
-  completed: boolean
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
 }
 
-fetchMock.get('https://jsonplaceholder.typicode.com/todos/1', {
-  userId: 1,
-  id: 1,
-  title: "delectus aut autem",
-  completed: false
-} as LoadTest);
+fetchMock.get(
+  "https://jsonplaceholder.typicode.com/todos/1",
+  {
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+  } as LoadTest,
+  { delay: 1000 }
+);
 
 test("My Greeter", () => {
   expect(__greeter("Carl")).toBe(HELLO_MESSAGE);
 });
 
 test("Mock http response", async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-  const result = await response.json()
-  
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const result = await response.json();
+
   expect(result).toEqual({
     userId: 1,
     id: 1,
     title: "delectus aut autem",
-    completed: false
-  })
-})
+    completed: false,
+  });
+});
+
+test("Test fetch with retry", async () => {
+  const response = await fetchWithRetry(
+    "https://jsonplaceholder.typicode.com/todos/1",
+    {},
+    { timeoutInSeconds: 2, tries: 2 }
+  );
+  const result = await response.json();
+
+  expect(result).toEqual({
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+  });
+});
